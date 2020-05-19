948b94e76e4fa22ad5bb028c840050a509c72f61
Avery Quinn <avery.quinn1493@gmail.com>
2020-05-19 14:57:54 -0400

    added function

--- app.py
+++ app.py
@@ -1,33 +1,55 @@
 # python app.py
 # set FLASK_APP=app.py
 
-from flask import Flask, render_template, request, redirect
+# Imports
+from flask import Flask, render_template, request, jsonify, redirect
+from cal import calculator
+
+
+# Instantiate App
 app = Flask(__name__) #name of module
 
+# Routes
 @app.route("/")
 def home():
     print("Visited Home Page")
+    print("------------------------------")
     return render_template("home.html")
 
 @app.route("/calculator")
-def calculator():
-    print("Visited Cal Page")
-    # title= request.form["title"]
-    # author= request.form["author"]
+def form():
+    print("Visited Calculator Form Page")
+    print("------------------------------")
     return render_template("calculator.html")
 
-@app.route("/calculator/output", methods=["POST"])
+@app.route("/calculator/output", methods=["POST", "GET"])
 def cal_output():
-    print("Visited Cal Output Page")
+    print("Visited Calculator Output Page")
+    print("------------------------------")
     device = request.form["device"] #<class 'str'>
     state = request.form["state"] #<class 'str'>
     hours = int(request.form["hours"]) #<class 'int'>
     days = int(request.form["days"]) #<class 'int'>
     x = {"device": device,"state": state,"hours": hours,"days": days}
-    print(type(device))
-    return x
+    y = calculator(device,state,hours,days)
+    return {"inputs": x, "outputs": y}
+
+@app.route("/<device>/<state>/<hours>/<days>", methods=["POST", "GET"])
+# Ceiling Fan/Missouri/8/5
+def cal(device,state,hours,days):
+    print("Visited Calculator URL Page")
+    print("------------------------------")
+    device = device
+    state = state
+    hours = int(hours)
+    days = int(days)
+    x = {"device": device,"state": state,"hours": hours,"days": days}
+    y = calculator(device,state,hours,days)
+    return {"inputs": x, "outputs": y}
+
 
 
 if __name__ == "__main__":
     app.run(debug=True)
+    print(calculator("Ceiling Fan","Missouri",8,5))
 
