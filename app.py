c0e9ce356ce3145fd93f29b0a1ae6fc997d638c6
Iulia Stremciuc <iuliastremciuc@gmail.com>
2020-06-02 19:18:33 -0500

    resnet model

--- app.py
+++ app.py
@@ -2,12 +2,25 @@
 # set FLASK_APP=app.py
 
 # Imports
-from flask import Flask, render_template, request, jsonify, redirect
+from flask import Flask, flash, request, redirect, url_for, render_template
+import urllib.request
 from cal import calculator
+from classification import predict
 from flask_cors import CORS
+from werkzeug.utils import secure_filename
+from resnet import res_model
+import os
 
 # Instantiate App
-app = Flask(__name__) #name of module
+# upload_file = '/path/to/the/uploads'
+# ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
+# path = os.makedirs(os.path.join(app.instance_path, 'uploads'), exist_ok=True)
+UPLOAD_FOLDER = './static/uploads'
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+# app.config['upload_file'] = upload_file #name of module
 CORS(app)
 
 # Routes
@@ -49,8 +62,43 @@ def cal(device,state,hours,days):
     return jsonify(y)
 
 
+def allowed_file(filename):
+	return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+	
+@app.route('/upload')
+def upload_form():
+	return render_template('upload.html')
+
+@app.route('/upload', methods=['POST'])
+def upload_image():
+	if 'file' not in request.files:
+		flash('No file part')
+		return redirect(request.url)
+	file = request.files['file']
+	if file.filename == '':
+		flash('No image selected for uploading')
+		return redirect(request.url)
+	if file and allowed_file(file.filename):
+		filename = secure_filename(file.filename)
+		file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+		#print('upload_image filename: ' + filename)
+		# flash('Image successfully uploaded and displayed')
+		return render_template('upload.html', filename=filename)
+	else:
+		# flash('Allowed image types are -> png, jpg, jpeg, gif')
+		return redirect(request.url)
+
+@app.route('/display/<filename>')
+def display_image(filename):
+	print('display_image filename: ' + filename)
+	return redirect(url_for('static', filename='uploads/' + filename), code=301)
+
+    
+
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
-    print(calculator("Ceiling Fan","Missouri",8,5))
+    # print(calculator("Ceiling Fan","Missouri",8,5))
 
