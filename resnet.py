d11ac6ec04a3829391413d92a8818bc8a0569f11
Iulia Stremciuc <iuliastremciuc@gmail.com>
2020-06-12 13:24:31 -0500

    new data, outputs

--- resnet.py
+++ resnet.py
@@ -4,8 +4,11 @@ import numpy as np
 from tensorflow.keras.applications.resnet50 import ResNet50
 from tensorflow.keras.preprocessing.image import load_img, img_to_array
 from tensorflow.keras.applications.resnet50 import preprocess_input, decode_predictions
+import os
+import pandas as pd 
 
-
+path_app_info = os.path.join(os.getcwd(),"data/app_info.csv")
+app_info = pd.read_csv(path_app_info)
 model = ResNet50(weights='imagenet')
 
 def res_model(file):
@@ -14,12 +17,15 @@ def res_model(file):
     image = np.expand_dims(image, axis=0)
     image = preprocess_input(image)
     pred = model.predict(image)
-    results = decode_predictions(pred)
-    imageID, label, prob = results[0][0]
-    data = [{"label": label, "probability": round(prob,2)}]
-    return data
+    label = decode_predictions(pred)
+    res = str(label[0][0][1])
+    detection = {str(label[0][0][-2:])}
+    info = app_info.loc[app_info['appliances'] == res, 'tips']
+    info = {str(info)}
+
+    return detection, info
 
 
-if __name__ == "__main__":
-    img =  './static/uploads/1658.jpg' 
-    print(res_model(img))
+# if __name__ == "__main__":
+#     img =  './static/uploads/1658.jpg' 
+#     print(res_model(img))
