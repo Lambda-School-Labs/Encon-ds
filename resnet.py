1634efc8abc2ce744640074d5bda0bed7b666fb6
Iulia Stremciuc <iuliastremciuc@gmail.com>
2020-06-16 16:32:15 -0500

    added IF statement to function

--- resnet.py
+++ resnet.py
@@ -6,28 +6,32 @@ from tensorflow.keras.preprocessing.image import load_img, img_to_array
 from tensorflow.keras.applications.resnet50 import preprocess_input, decode_predictions
 import os
 import pandas as pd 
+from matplotlib import pyplot as plt 
 
 path_app_info = os.path.join(os.getcwd(),"data/app_info.csv")
 app_info = pd.read_csv(path_app_info)
-model = ResNet50(weights='imagenet')
+resnet = ResNet50(weights='imagenet')
 
 def res_model(file):
-    image = load_img(file, target_size=(224, 224))
-    image = img_to_array(image)
-    image = np.expand_dims(image, axis=0)
-    image = preprocess_input(image)
-    pred = model.predict(image)
-    label = decode_predictions(pred)
-    res = label[0][0][1]
+    img = load_img(file, target_size=(224, 224))
+    img = img_to_array(img)
+    img = np.expand_dims(img, axis=0)
+    plt.imshow(np.uint8(img[0]))
+    img = preprocess_input(img.copy())
+    predictions = resnet.predict(img)
+    label = decode_predictions(predictions)
+    res = str(label[0][0][1])
     detection = {str(label[0][0][1])}
     info = app_info.loc[app_info['appliances'] == res, 'tips']
-    info = info.to_json()
+    if info.empty:
+      return {"not in our inventory take a better picture"}
+    else:
+      return {str(info)}
+      
 
-    return [detection, info]
 
 
 
-
-# if __name__ == "__main__":
-#     img =  './static/uploads/.jpg' 
-#     print(res_model(img))
+if __name__ == "__main__":
+    img =  './static/uploads/best-refrigerators-1571069203.jpg' 
+    print(res_model(img))
